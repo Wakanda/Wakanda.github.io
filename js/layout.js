59dd46878e72918aef4b1008d59423029dd86903
Hamza HIKMAT <hamzahik@gmail.com>
2017-03-16 17:42:20 +0100

    rollback

---
+++ js/layout.js
@@ -0,0 +1,11 @@
+/**
+ * Smooth scroll
+ */
+$(document).ready(function() {
+  $('.js-scrollTo').on('click', function() {
+    var page = $(this).attr('href');
+    var speed = 750;
+    $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+    return false;
+  });
+});
