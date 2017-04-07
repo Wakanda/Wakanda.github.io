7f7735590c95b3515cedd57974c857f898ac4a00
Adil Echcharafi <charafi.adil@gmail.com>
2017-04-07 15:24:52 +0100

    Version 1.1.4

--- js/main.js
+++ js/main.js
@@ -1743,11 +1743,11 @@ var stableLinks = communityLinks("stable");
 var previewLinks = communityLinks("preview");
 var platform = getPlatform();
 var entrepriseLink = {
-    macOS: "//bo.wakanda.io/productionchannel/2.0.1/mac/wakanda-enterprise-all_2.0.1_x64.dmg",
-    win64: "//bo.wakanda.io/productionchannel/2.0.1/windows/wakanda-enterprise-all_2.0.1_x64.msi",
-    //win32: "//bo.wakanda.io/productionchannel/2.0.1/windows/wakanda-enterprise-all_2.0.1_x86.msi",
-    linux32: "//bo.wakanda.io/productionchannel/2.0.1/linux/wakanda-enterprise-server_2.0.1_i386.deb",
-    linux64: "//bo.wakanda.io/productionchannel/2.0.1/linux/wakanda-enterprise-server_2.0.1_amd64.deb"
+    macOS: "//bo.wakanda.io/productionchannel/1.1.4/mac/wakanda-enterprise-all_1.1.4_x64.dmg",
+    win64: "//bo.wakanda.io/productionchannel/1.1.4/windows/wakanda-enterprise-all_1.1.4_x64.msi",
+    win32: "//bo.wakanda.io/productionchannel/1.1.4/windows/wakanda-enterprise-all_1.1.4_x86.msi",
+    linux32: "//bo.wakanda.io/productionchannel/1.1.4/linux/wakanda-enterprise-server_1.1.4_i386.deb",
+    linux64: "//bo.wakanda.io/productionchannel/1.1.4/linux/wakanda-enterprise-server_1.1.4_amd64.deb"
 };
 
 
