3fdc35ed3114d5c0d51bc46fd788e43454fa5790
Adil Echcharafi <charafi.adil@gmail.com>
2018-04-25 16:00:01 +0100

    Wakanda new version

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -105,9 +105,9 @@ $("#community-dl").click(function() {
  */
 
 var versionLinks = { 
-    communiy_stable: "2.4.0",
+    communiy_stable: "2.5.0",
     //preview: "2.0.1",
-    enterprise: "2.4.0"
+    enterprise: "2.5.0"
 };
 
 var baseCommunityLink = "https://github.com/Wakanda/wakanda-digital-app-factory/releases/download/";
